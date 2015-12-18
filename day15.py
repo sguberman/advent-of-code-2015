d38734b7b70d2cceac12d0e7b6cf540c2696797c
Seth Guberman <sguberman@gmail.com>
2015-12-18 18:03:36 +0000

    complete day 15 part 2

--- day15.py
+++ day15.py
@@ -4,13 +4,14 @@ import itertools
 
 class TestExampleRecipe(unittest.TestCase):
 
-    ingredients = ((-1, -2, 6, 3),
-                   (2, 3, -2, -1))
+    ingredients = ((-1, -2, 6, 3, 8),
+                   (2, 3, -2, -1, 3))
 
     mix = (44, 56)
 
     def test_best_recipe(self):
         self.assertEqual(best_recipe('day15.test'), 62842880)
+        self.assertEqual(best_recipe('day15.test', calories=500), 57600000)
 
     def test_read_ingredients(self):
         self.assertEqual(read_ingredients('day15.test'), self.ingredients)
@@ -26,7 +27,7 @@ class TestExampleRecipe(unittest.TestCase):
         self.assertNotIn(bad_recipe, possible_recipes(self.ingredients))
 
 
-def score(recipe):
+def score(recipe, calories=None):
     mix, ingredients = recipe
     properties = zip(*ingredients)
     cookie = []
@@ -38,16 +39,18 @@ def score(recipe):
 
     if any(prop < 0 for prop in cookie):
         return 0
+    elif calories and cookie[-1] != calories:
+        return 0
     else:
         score = 1
-        for prop in cookie:
+        for prop in cookie[:-1]:
             score *= prop
         return score
 
 
-def best_recipe(ingredients_file):
+def best_recipe(ingredients_file, calories=None):
     ingredients = read_ingredients(ingredients_file)
-    return max(score(recipe) for recipe in possible_recipes(ingredients))
+    return max(score(recipe, calories) for recipe in possible_recipes(ingredients))
 
 
 def read_ingredients(ingredients_file):
@@ -62,7 +65,7 @@ def read_ingredients(ingredients_file):
 def parse_ingredient(line):
     n, _, c, _, d, _, f, _, t, _, cal = line.split()
     name = n.strip(':').lower()
-    stats = tuple(int(num.strip(',')) for num in (c, d, f, t))
+    stats = tuple(int(num.strip(',')) for num in (c, d, f, t, cal))
     return name, stats
 
 
@@ -80,4 +83,5 @@ def parts(n, length=None):
 
 if __name__ == '__main__':
     # unittest.main()
-    print best_recipe('day15.input')
+    # print best_recipe('day15.input')
+    print best_recipe('day15.input', calories=500)
