0d0decde0bb9c371f90bfec9ec90541808e0d582
Seth Guberman <sguberman@gmail.com>
2015-12-18 19:15:08 +0000

    complete day 17

---
+++ day17.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+from collections import defaultdict
+
+
+with open('day17.input', 'r') as containersfile:
+    containers = sorted([int(line) for line in containersfile])
+
+solutions = defaultdict(list)
+for n in range(len(containers)):
+    for combo in combinations(containers, n):
+        if sum(combo) == 150:
+            solutions[n].append(combo)
+
+print len(solutions[min(solutions.keys())])
