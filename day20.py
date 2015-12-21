6967475e5acc5e09f144036b93026d48a075d876
Seth Guberman <sguberman@gmail.com>
2015-12-21 19:11:19 +0000

    complete day 20 part 2

--- day20.py
+++ day20.py
@@ -1,13 +1,27 @@
+from collections import defaultdict
+
+
 def factors(n):
-    for i in range(1, int(n**0.5) + 1):
+    for i in xrange(1, int(n**0.5) + 1):
         if n % i == 0:
             yield i
             yield n // i
 
-minpresents = 34000000
 
-for i in range(1, minpresents/10 + 1):
-    presents = 10 * sum(set(factors(i)))
+minpresents = 34000000
+maxdeliveries = 50
+presents_per_elf = 11
+deliveries = defaultdict(int)
+
+for i in xrange(1, minpresents // presents_per_elf + 1):
+    presents = 0
+    elves = set(factors(i))
+    for elf in elves:
+        if deliveries[elf] < maxdeliveries:
+            deliveries[elf] += 1
+            presents += elf
+    presents *= presents_per_elf
     if presents >= minpresents:
-        print i
         break
+
+print i, presents
