4820cbe7d3f33b1e1f7c3e70a9ec9aa79ba6e752
Seth Guberman <sguberman@gmail.com>
2016-01-04 14:00:13 -0500

    complete day 24

--- day24.py
+++ day24.py
@@ -1,41 +1,19 @@
 from itertools import chain, combinations
+from operator import mul
 
 
-def powerset(iterable):
+def powerset(iterable):  # from https://docs.python.org/2/library/itertools.html
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in xrange(len(s)+1))
 
 
-packages = []
-with open('day24.input', 'r') as f:
-    for line in f:
-        packages.append(int(line.strip()))
+packages = sorted([int(line) for line in open('day24.input', 'r')], reverse=True)
 
-# test packages
-packages = (1, 2, 3, 4, 5, 7, 8, 9, 10, 11)
+n_groups = 4
+target_weight = sum(packages) / n_groups
 
+first_groups = (g for g in powerset(packages) if sum(g) == target_weight)
+quantum_entanglements = (reduce(mul, g) for g in first_groups)
 
-solutions = []
-group1s = powerset(reversed(sorted(packages)))
-for group1 in group1s:
-    if len(group1) in (0, len(packages), len(packages) - 1):
-        continue
-    if len(solutions) > 0:
-        break
-    g1sum = sum(group1)
-    g1qe = reduce(lambda x, y: x*y, group1, 1)
-    others = tuple(p for p in packages if p not in group1)
-    group2s = powerset(others)
-    for group2 in group2s:
-        if len(group2) in (0, len(others)):
-            continue
-        g2sum = sum(group2)
-        group3 = tuple(p for p in others if p not in group2)
-        # print group1, group2, group3
-        g3sum = sum(group3)
-        if g1sum == g2sum == g3sum:
-            solution = (len(group1), g1qe, g1sum, group1, group2, group3)
-            solutions.append(solution)
-
-print sorted(solutions)[0]
+print next(quantum_entanglements)
